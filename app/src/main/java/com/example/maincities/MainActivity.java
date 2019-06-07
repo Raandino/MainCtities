6292a8302bda365e9873cc15206e64463ebca4d2
Raandino <aandino186@gmail.com>
2019-06-06 23:43:17 -0600

    Agrege mas request para los otros datos

--- app/src/main/java/com/example/maincities/MainActivity.java
+++ app/src/main/java/com/example/maincities/MainActivity.java
@@ -16,12 +16,16 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-//sirvio
+
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView tiempo;//Aqui se almacena el tiempo
     private TextView t_descrip;//Aqui se almacena descripccion del tiempo
+    private TextView temperatura;
     private RequestQueue mQueue;//
+    DecimalFormat df = new DecimalFormat("#.00");
     Button GoLon,GoNew,GoMan;
 
 
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         tiempo = findViewById(R.id.tiempo);//encontrando el id del ViewText
         t_descrip =findViewById(R.id.t_descrip);//encontrando id del ViewText
+        temperatura =findViewById(R.id.temperatura);//encontrando id del ViewText
         Button GoLon = findViewById(R.id.button_parse);//encontrando id del Button
 
         mQueue = Volley.newRequestQueue(this);//instancia de requestQueue
@@ -90,25 +95,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-
                 /*------------------RESPUESTA JSON A LOS ARRAYS------------------*/
                     JSONArray jsonWea = response.getJSONArray("weather");//Respuesta a arreglo json "weather"
-                    JSONArray jsonMain = response.getJSONArray("main");//Respuesta a arreglo json "main"
-
-
-
-
                     JSONObject weather = jsonWea.getJSONObject(0);
-
-
-                    String main =  weather.getString("main");//main es el tiempo del lugar
+                    String main_tiempo =  weather.getString("main");//main es el tiempo del lugar
                     String description =  weather.getString("description");//description es la descrip del tiempo
-                    tiempo.append(main);///cambia el valor del TextView
+                    tiempo.append(main_tiempo);///cambia el valor del TextView
                     t_descrip.append(description);///cambia el valor del TextView
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                } catch (JSONException error) {
+                    error.printStackTrace();
                 }
 
             }
@@ -119,8 +114,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        final JsonObjectRequest request2 = new JsonObjectRequest(Request.Method.GET, url, null
+                , new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    /*------------------RESPUESTA JSON A LOS ARRAYS------------------*/
+                    JSONObject main = response.optJSONObject("main");//Respuesta a arreglo json "main" donde esta la temperatura
+                    double temp_kelvin = main.getDouble("temp");
+                    double temp_celsius=temp_kelvin-273;
+                    String temp_c = df.format(temp_celsius);
+                    temperatura.append(temp_c+"°C");
+                } catch (JSONException error) {
+                    error.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request2);
         mQueue.add(request);
     }
+
+
 
 
 
